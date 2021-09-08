9403b2ca92a452048c3df9f705df93420febc229
Max <maxthorstorp@gmail.com>
2021-09-08 10:20:20 +0200

    IntroFirstTask Solution

---
+++ IntroFirstTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IntroFörstaUppgiften
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej, knappa in två siffror för att dividera!");
+            float firstNumber = float.Parse(Console.ReadLine());
+            float secondNumber = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Resultatet av " + firstNumber + " dividerat med " + secondNumber + " är:" + firstNumber / secondNumber);
+
+        }
+    }
+}
